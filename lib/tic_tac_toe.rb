eb8530820473ac7733e26310776a9c92d65d919d
Miriam Castillo <castillo.m.mc@gmail.com>
2018-03-01 04:04:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,8 +34,20 @@ def position_taken?(index)
   @board[index] != " " && @board[index] != ""
 end
 
-def valid_move?(board, index)
+def valid_move?(index)
 index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
 end
 
 def turn_count
@@ -43,7 +55,7 @@ def turn_count
   end
 
 def current_player(board)
-    turn_count(board).even? ? "X" : "O"
+    turn_count.even? ? "X" : "O"
   end
 
   def won?(board)
